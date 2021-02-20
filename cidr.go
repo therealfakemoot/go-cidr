eef89db0d3610f7ea0c914c60c557431113be8db
Nick Dumas <drakonik@gmail.com>
2021-02-19 20:15:56 -0500

    most of the implementation

--- cidr.go
+++ cidr.go
@@ -1,5 +1,27 @@
 package cidr
 
 import (
-// "net"
+	"encoding/binary"
+	"net"
 )
+
+func Range(r string) ([]net.IP, error) {
+	var ns []net.IP
+	ip, n, err := net.ParseCIDR("192.145.34.123/16")
+	if err != nil {
+		return nil, err
+	}
+	ipInt := binary.BigEndian.Uint32(ip.To4())
+	mask := binary.BigEndian.Uint32(n.Mask)
+
+	// start the ++ here
+	// when do we stop?
+	x := ipInt & mask
+	ipBytes := make([]byte, 4)
+	for i := x; i > 0; i++ {
+		binary.BigEndian.PutUint32(ipBytes, i)
+		ns = append(ns, net.IP(ipBytes))
+	}
+
+	return ns, nil
+}
--- cidr_test.go
+++ cidr_test.go
@@ -1,1 +1,44 @@
 package cidr
+
+import (
+	"testing"
+)
+
+func TestRange(t *testing.T) {
+	t.Run("everything", func(t *testing.T) {
+		t.Log("starting the big one")
+		ns, err := Range("0.0.0.0/32")
+		if err != nil {
+			t.Logf("error generating range: %s", err)
+			t.Fail()
+		}
+
+		if len(ns) != 4294967296 {
+			t.Logf("wrong # of addresses: %d", len(ns))
+			t.Fail()
+		}
+		t.Log("done with the big one")
+	})
+	t.Run("valid ranges", func(t *testing.T) {
+		inputs := []struct {
+			input    string
+			expected int
+		}{
+			// formula is 2 ^ addrLength - prefixLength
+			{"127.0.0.1/18", 16384},
+			{"10.0.0.0/24", 256},
+		}
+
+		for _, tt := range inputs {
+			ns, err := Range(tt.input)
+			if err != nil {
+				t.Logf("error generating range: %s", err)
+				t.Fail()
+			}
+			if len(ns) != tt.expected {
+				t.Logf("wrong # of addresses: %d", len(ns))
+				t.Fail()
+			}
+		}
+	})
+}
